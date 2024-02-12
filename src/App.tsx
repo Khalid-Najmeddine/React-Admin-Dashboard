12549580089ae4795d68cf9a4506986940de1478
Khalid Najmeddine <knajm574@gmail.com>
2024-02-12 15:03:00 -0700

    feat: client-side routing + defining routes with createBrowserRouter that display components and Pages as part of Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,46 @@
-import Home from "./pages/Home /Home"
+import Home from "./pages/Home/Home"
+import {createBrowserRouter,RouterProvider,Outlet} from "react-router-dom";
+import Users from "./pages/Users/Users";
+import Products from "./pages/Products/Products";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+import Menu from "./components/Menu/Menu";
+import Login from "./pages/Login/Login";
 
 function App() {
 
+  const Layout = () => {
+    return (
+      <div className="main">
+        <Navbar/>
+        <div className="container">
+          <div className="menu-Container">
+            <Menu/>
+          </div>
+          <div className="content-Container">
+            <Outlet/>
+          </div>
+        </div>
+        <Footer/>
+      </div>
+    )
+  }
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout/>,
+      children: [
+        {path: "/", element: <Home/>},
+        {path: "/users", element: <Users/>},
+        {path: "/products", element: <Products/>}
+      ]
+    },
+    {path:"/login", element: <Login/>}
+  ]);
+
   return (
-    <div><Home/></div>
+    <RouterProvider router={router}/>
   )
 }
 
