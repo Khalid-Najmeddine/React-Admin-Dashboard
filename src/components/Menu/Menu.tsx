d01bdc3924555839be9ae8889a7cce8966b3f30b
Khalid Najmeddine <knajm574@gmail.com>
2024-02-12 16:52:10 -0700

    feat: Menu Design

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,9 +1,23 @@
 import "./Menu.scss"
+import { Link } from 'react-router-dom';
+import {menu} from "../../data"
 
 const Menu = () => {
   return (
-    <div className="menu">Menu</div>
-  )
-}
+    <div className="menu">
+     { menu.map((item) => (
+      <div className="item" key={item.id}>
+          <span className="title">{item.title}</span>
+          {item.listItems.map((listItem) => (
+            <Link to="/" className="list-Item" key={listItem.id}>
+              <img src={listItem.icon} alt="" />
+              <span className="list-Item-Title">{listItem.title}</span>
+            </Link>
+          ))}
+        </div>
+        ))}
+    </div>
+  );
+};
 
 export default Menu
