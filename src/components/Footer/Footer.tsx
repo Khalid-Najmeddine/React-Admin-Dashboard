ea08387fe3ef99511ce9bd43662f14d65179c882
Khalid Najmeddine <knajm574@gmail.com>
2024-02-12 16:16:03 -0700

    feat: Footer Design

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,7 +2,10 @@ import "./Footer.scss"
 
 const Footer = () => {
   return (
-    <div className="footer">Footer</div>
+    <div className="footer">
+      <span>Khalid's Admin Dashboard</span>
+      <span> © 2024 Khalid's Admin Dashboard</span>
+    </div>
   )
 }
 
